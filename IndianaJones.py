8ddbb0a158bd685b25a29ac74bdb7495285cc55a
Timfinity4 <168996454+JoelBDirtbike@users.noreply.github.com>
2025-07-24 19:57:51 -0700

    Update IndianaJones.py

--- IndianaJones.py
+++ IndianaJones.py
@@ -1,8 +1,13 @@
 from PortMap25 import *
+from pybricks.hubs import InventorHub
+from pybricks.tools import run_task
+
+hub = InventorHub()
+speaker = hub.speaker
 
 async def Indiana_Jones():
-    await speaker.play_notes([
-        "E4/5", "F4/16", "G4/8", "C5/3", "D4/5", "E4/16", "F4/2", "G4/5", "A5/16", "B5/8", "F5/2", "A5/5", "B5/16", "C5/4", "D5/4", "E5/4", "E4/5", "D4/16"  # long rest to end
+    await speaker.play_notes([                                                                                                                                                                                                                                                                                                                  # * repition begins                                                                                                                                                                                                                                               * second ending                                                    
+        "E4/5", "F4/16", "G4/8", "C5/2_", "C5/8", "D4/5", "E4/16", "F4/2", "G4/5", "A5/16", "B5/8", "F5/2_", "F5/8", "A5/5", "B5/16", "C5/4", "D5/4", "E5/4", "E4/5", "D4/16", "G4/8", "C5/2_", "C5/8", "D5/5", "E5/16", "F5/2", "R/4", "G4/5", "G4/16", "E5/4", "D5/5", "G4/16", "E5/4", "D5/5", "G4/16", "E5/4", "D5/5", "G4/16", "E5/8", "D5/8", "E4/5", "F4/16", "G4/8", "C5/2_", "C5/8", "D4/5", "E4/16", "F4/2", "G4/5", "A5/16", "B5/8", "F5/2_", "F5/8", "A5/5", "B5/16", "C5/4", "D5/4", "E5/4", "E4/5", "D4/16", "G4/8", "C5/2_", "C5/8", "D5/5", "E5/16", "F5/2", "R/4", "G4/5", "G4/16", "E5/4", "D5/5", "G4/16", "E5/4", "D5/5", "G4/16", "E5/4", "D5/5", "G4/16", "E5/8", "D5/8", "E4/5", "G4/16", "F4/2."
     ], tempo=120)
 
 run_task(Indiana_Jones())
