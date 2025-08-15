3933bb90bff43cb00bd04688d3e955abcb7d4a7f
Timfinity4 <168996454+JoelBDirtbike@users.noreply.github.com>
2025-08-14 19:56:41 -0700

    Create Liam4.py

---
+++ Liam4.py
@@ -0,0 +1,9 @@
+from QuadBotPortMap import *
+#7,1
+drivebase.straight(600)
+drivebase.turn(-110)
+drivebase.turn(70)
+drivebase.straight(-50)
+drivebase.turn(-30)
+drivebase.straight(65)
+drivebase.turn(20) 
