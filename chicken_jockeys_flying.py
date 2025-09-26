1d457a8c0c8a323ae419de972fe81501c6030e02
Timfinity4 <168996454+JoelBDirtbike@users.noreply.github.com>
2025-09-25 18:10:32 -0700

    Create chicken_jockeys_flying.py

---
+++ chicken_jockeys_flying.py
@@ -0,0 +1,15 @@
+from QuadBotPortMap import *
+drivebase.settings(straight_speed=300)
+drivebase.straight(610)
+drivebase.turn(50)
+drivebase.straight(30)
+drivebase.straight(-5)
+drivebase.turn(-55)
+drivebase.straight(60)
+drivebase.turn(-30)
+drivebase
+
+
+
+
+
