3e5d08e6798477cb2052a2d2571ab2d0a91a8289
Timfinity4 <168996454+JoelBDirtbike@users.noreply.github.com>
2025-10-05 14:07:51 -0700

    Create sandpull.py

---
+++ sandpull.py
@@ -0,0 +1,10 @@
+from QuadBotPortMap import *
+drivebase.straight(-450)
+drivebase.turn(-22.2)
+drivebase.straight(-115)
+drivebase.turn(20)
+drivebase.straight(-50)
+drivebase.turn(20)
+drivebase.turn()
+drivebase.straight(450)
+#1,3
