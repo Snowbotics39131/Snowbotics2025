44d590a3e01af64627f78f254b69becbe7a0bab6
Timfinity4 <168996454+JoelBDirtbike@users.noreply.github.com>
2025-10-15 20:37:37 -0700

    added robot

--- forge.py
+++ forge.py
@@ -1,15 +1,19 @@
 from QuadBotPortMap import *
-drivebase.settings(straight_speed=500)
+drivebase.settings(straight_speed=300, straight_acceleration=500)
 drivebase.straight(610)
-drivebase.turn(40)
+drivebase.turn(40) #40
 drivebase.straight(55)
-drivebase.turn(-15)
+drivebase.turn(-15) #25
 drivebase.straight(-85)
-drivebase.turn(-20)
-drivebase.straight(125)
-drivebase.turn(-25)
-drivebase.turn(25)
-drivebase.straight(-1000)
+drivebase.turn(-20) #5
+drivebase.straight(145)
+drivebase.turn(-25) #-20
+drivebase.straight(-5)
+drivebase.turn(25) #5
+drivebase.straight(-20)
+drivebase.turn(-95)
+drivebase.straight(-200)
+wait(1000)
 # drivebase.turn(-3)
 # drivebase.straight(6)
 # drivebase.turn(-15)
---
+++ robot.py
@@ -0,0 +1,11 @@
+import QuadBotPortMap
+from pybricks.tools import hub_menu
+mission = hub_menu(1,2)
+print(mission)
+
+if mission == 1:
+    import forge
+if mission == 2:
+    import M10_KrithvikBlue
+
+
