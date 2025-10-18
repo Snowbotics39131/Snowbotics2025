be9faa9de9d4144a4409e82c70ca5f59c206f41e
Timfinity4 <168996454+JoelBDirtbike@users.noreply.github.com>
2025-10-18 07:25:21 -0700

    Update robot.py

--- robot.py
+++ robot.py
@@ -1,11 +1,11 @@
 import QuadBotPortMap
 from pybricks.tools import hub_menu
-mission = hub_menu(1,2)
+mission = hub_menu(1,2,3)
 print(mission)
 
 if mission == 1:
     import forge
 if mission == 2:
     import M10_KrithvikBlue
-
-
+if mission == 3:
+    import CraneMission
