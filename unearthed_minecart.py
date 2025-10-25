5fc353377563050b9e26b268fc9bdec266c02834
Tsang-man <168996447+Tsang-man@users.noreply.github.com>
2025-10-24 19:36:37 -0600

    Update unearthed_minecart.py

--- unearthed_minecart.py
+++ unearthed_minecart.py
@@ -1,11 +1,3 @@
-from pybricks.hubs import InventorHub
-from pybricks.pupdevices import Motor, ColorSensor, UltrasonicSensor
-from pybricks.parameters import Button, Color, Direction, Port, Side, Stop
-from pybricks.robotics import DriveBase
-from pybricks.tools import wait, StopWatch
-
-hub = InventorHub()
-
 from QuadBotPortMap import *
 
 
