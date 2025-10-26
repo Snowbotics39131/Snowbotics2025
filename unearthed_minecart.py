8d611c133b61847bc087a0897fb736a3c06ea26c
JoelBDirtbike <168996454+JoelBDirtbike@users.noreply.github.com>
2025-10-26 16:45:21 -0600

    Update unearthed_minecart.py

--- unearthed_minecart.py
+++ unearthed_minecart.py
@@ -1,23 +1,23 @@
 from QuadBotPortMap import *
 
 async def drive_code():
-#shift(2)
-#use_attachment(-150000, 800)
-await drivebase.settings(straight_speed=500)
-await multitask (drivebase.straight(250), shift(2))
-await use_attachment(70000, 600)
-await use_attachment(-70000, 600)
-await shift(3)
-await use_attachment(-70000, 1000)
-await drivebase.turn(45)
-await drivebase.straight(100)
-await drivebase.turn(90)
-await drivebase.straight(150)
-await drivebase.turn(-45)
-await drivebase.straight(1000)
+    #shift(2)
+    #use_attachment(-150000, 
+    drivebase.settings(straight_speed=500)
+    await multitask (drivebase.straight(250), shift(2))
+    await use_attachment(-70000, 600)
+    await use_attachment(70000, 600)
+    await shift(3)
+    await use_attachment(70000, 1000)
+    await drivebase.turn(45)
+    await drivebase.straight(100)
+    await drivebase.turn(90)
+    await drivebase.straight(150)
+    await drivebase.turn(-45)
+    await drivebase.straight(1000)
 
 
-
+run_task(drive_code())
 
 
 
