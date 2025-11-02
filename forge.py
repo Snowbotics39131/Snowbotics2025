dfd3b3289c040fe81d3a53a38656624fb4e106a1
Timfinity4 <168996454+JoelBDirtbike@users.noreply.github.com>
2025-11-02 15:10:14 -0800

    Update forge.py

--- forge.py
+++ forge.py
@@ -1,27 +1,26 @@
 from QuadBotPortMap import *
+# right 3,1
 drivebase.settings(straight_speed=300, straight_acceleration=400)
 run_task(shift(1))
 drivebase.straight(610)
-drivebase.turn(40) #40
-drivebase.straight(55)
-drivebase.turn(-15) #25
-drivebase.straight(-80)
-drivebase.turn(-20) #5
-drivebase.straight(145)
-drivebase.turn(-25) #-20
-drivebase.straight(-5)
-drivebase.turn(25) #5
+drivebase.turn(42) #42
+drivebase.straight(40)
+drivebase.turn(-22) #20
+drivebase.straight(-50)
+drivebase.turn(-18) #2
+drivebase.straight(115)
+drivebase.turn(-27) #-25
+#drivebase.turn(25) #5
 drivebase.straight(-20)
-drivebase.turn(-95) #-90
-drivebase.straight(-200)
-wait(100)
-drivebase.straight(350)
+drivebase.turn(-65)
+drivebase.straight(-210)
+drivebase.straight(370)
 drivebase.turn(30)
 drivebase.straight(200)
 drivebase.turn(-30)
-drivebase.straight(240)
+drivebase.straight(230)
 drivebase.turn(45)
-run_task(use_attachment(-300, 10000))
+run_task(use_attachment(-250, 10000))
+drivebase.settings(straight_speed=9000, straight_acceleration=900)
 drivebase.straight(-245)
-
-
+drivebase.straight(245)
