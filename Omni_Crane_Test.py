f7eded5ffd9b934ba0ad1aeb19c4bbf27b9f87d3
Tsang-man <168996447+Tsang-man@users.noreply.github.com>
2025-11-09 15:42:51 -0700

    Add files via upload

---
+++ Omni_Crane_Test.py
@@ -0,0 +1,12 @@
+from QuadBotPortMap import *
+
+async def drive_code():
+    await shift(2)
+    await drivebase.use_attachment(-5000, 300)
+    await drivebase.use_attachment(5000, 300)
+    await shift(3)
+    await drivebase.use_attachment(-5000, 300)
+    await drivebase.use_attachment(5000, 300)
+
+if __name__ == "__main__": #run on file run but not import
+    run_task(drive_code())
