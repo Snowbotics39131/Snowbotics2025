b9f834347a3669578b6a3ee32719ceee36d98513
JoelBDirtbike <168996454+JoelBDirtbike@users.noreply.github.com>
2025-11-09 15:38:45 -0700

    Update TheMostAwesome.py

--- TheMostAwesome.py
+++ TheMostAwesome.py
@@ -62,7 +62,6 @@ from QuadBotPortMap import *
 
 
 
-
 #0,6
 
 
@@ -89,13 +88,44 @@ from QuadBotPortMap import *
 
 
 
+#drivebase.straight(650)
+#drivebase.straight(-130)
+#drivebase.straight(197.5)
+#drivebase.turn(-45)
+#drivebase.straight(200)
+#drivebase.turn(-65)
+#drivebase.straight(-120)
+#drivebase.turn(-95)
+#drivebase.settings(straight_speed=400)
+#drivebase.curve(900,50)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 drivebase.straight(650)
-drivebase.straight(-120)
-drivebase.straight(170)
+drivebase.straight(-130)
+drivebase.straight(197.5)
 drivebase.turn(-45)
 drivebase.straight(200)
 drivebase.turn(-65)
-drivebase.straight(-120)
+drivebase.straight(-170)
 drivebase.turn(-95)
 drivebase.settings(straight_speed=400)
 drivebase.curve(900,50)
