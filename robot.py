4d57c65c5372b1bcdce7b9979bf04f59a74a5213
JoelBDirtbike <168996454+JoelBDirtbike@users.noreply.github.com>
2025-11-12 19:45:40 -0700

    Update robot.py

--- robot.py
+++ robot.py
@@ -1,6 +1,6 @@
 import QuadBotPortMap
 from pybricks.tools import hub_menu
-mission = hub_menu(1,2,3)
+mission = hub_menu(1,2,3,4,5)
 print(mission)
 
 if mission == 1:
@@ -9,3 +9,7 @@ if mission == 2:
     import M10_KrithvikBlue
 if mission == 3:
     import CraneMission
+if mission == 4:
+    import Liam4
+if mission == 5:
+    import sand_3
