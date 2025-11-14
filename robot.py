5607b3d7f5492e794705842df0c53ce5db463202
JoelBDirtbike <168996454+JoelBDirtbike@users.noreply.github.com>
2025-11-13 18:55:16 -0700

    Update robot.py

--- robot.py
+++ robot.py
@@ -1,22 +1,21 @@
 import QuadBotPortMap
 from pybricks.tools import hub_menu
-mission = hub_menu(1,2,3,4,5)
+mission = hub_menu(1,2,3,4)
 print(mission)
 
 #(forge)_alignment=-----FrontLeft-----15x, 8y_RIGHT
 #(M10_KrithvikBlue)_alignment=-----FrontLeft-----14x, 11y_RIGHT
-#(CraneMission)_alignment=-----FrontLeft-----
-#(TheMostAwesome)_alignment=-----FrontLeft-----
-#(sand_3)_alignment=-----FrontLeft-----8x, 1y_LEFT
+#(CraneMission)_alignment=-----FrontLeft-----XCancelX
+#(TheMostAwesome)_alignment=-----FrontLeft-----6x, 7yL
+#(sand_3)_alignment=-----FrontLeft-----4x, 1y_LEFT
 
 if mission == 1:
     import forge
 if mission == 2:
     import M10_KrithvikBlue
+# if mission == 3:
+#     import CraneMission
 if mission == 3:
-    import CraneMission
-    run_task(drive_code())
-if mission == 4:
     import TheMostAwesome
-if mission == 5:
+if mission == 4:
     import sand_3
